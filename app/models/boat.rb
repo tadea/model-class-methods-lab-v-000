491872716cfaf7b5d702102b980300619360c310
Tadea Simunovic <tsimunoviczg@gmail.com>
2019-03-22 16:52:14 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -24,6 +24,6 @@ class Boat < ActiveRecord::Base
   end
 
   def self.sailboats
-    self.join(:classifications).where({classifications: {name: "sailboat"}})
+    joins(:classifications).where({classifications: {name: "sailboat"}})
   end
 end
