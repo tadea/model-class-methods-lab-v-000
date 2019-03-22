4bacec1a2e7d37603229edc0e56853199069a27e
Tadea Simunovic <tsimunoviczg@gmail.com>
2019-03-22 16:27:51 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,8 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.first(5)
+  end
 end
